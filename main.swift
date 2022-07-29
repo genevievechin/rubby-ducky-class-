d9fa27eb3a443695d972ea8a1c09ae1ec5fe9c7f
Genevieve Chin <64508351+genevievechin@users.noreply.github.com>
2022-07-29 16:41:58 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+//  🍦 Declaring a class
+
+//  🍦 Storing properties in the code block
+
+
+//  🍦 Creating an object
+
+
+//  🍦 Printing an object
+
+
+//  🍦 Using dot notation to access properties
+
+
+
+
+// email faith 
+// fill out my meeting tracker form before lunch 
